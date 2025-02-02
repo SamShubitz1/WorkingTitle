eb92b4d5689a503964c59c5e2a2fc3eeb0562d5b
Sam Shubitz <shubitzs@gmail.com>
2025-02-02 01:19:54 -0600

    wow

--- Scenes/Battle/Cursor/cursor.gd
+++ Scenes/Battle/Cursor/cursor.gd
@@ -1,6 +1,20 @@
 extends TextureRect
 
-var offset = Vector2i(110, 11)
+enum MenuType {
+	OPTIONS,
+	ABILITIES
+}
 
+var current_menu_type = MenuType.OPTIONS
+var options_offset = Vector2i(110, 11)
+var abilities_offset = Vector2i(490, 21)
+
+#Enum types become their index?
 func move_cursor(position: Vector2i) -> void:
-	self.position = position + offset
+	if current_menu_type == 0:
+		self.position = position + options_offset
+	elif current_menu_type == 1:
+		self.position = position + abilities_offset
+
+func set_menu_type(type: MenuType):
+	current_menu_type = type
--- Scenes/Battle/Menu/battle_menu.gd
+++ Scenes/Battle/Menu/battle_menu.gd
@@ -1,65 +1,71 @@
 extends Control
 
+enum MenuType {
+	OPTIONS,
+	ABILITIES
+}
+
 @onready var cursor = $Cursor
-@onready var options_list = $Options.get_children().slice(1)
-@onready var abilities_list = $Abilities.get_children().slice(3)
-@onready var descriptions_list = $Descriptions.get_children()
+@onready var options_menu = $Options.get_children().slice(1)
+@onready var abilities_menu = $Abilities.get_children().slice(3)
+@onready var descriptions_menu = $Descriptions.get_children()
 
 var character_abilities: Array = ["Rock", "Paper", "Scissors"]
 
 var selected_index: int = 0
-var selected_button: Button = null
+var selected_button: Button
 
-var menus: Array = [options_list, abilities_list]
+var menus: Array = []
 var selected_menu_index: int = 0
-var current_menu = null
+var current_menu: Array
 
 func _ready() -> void:
+	menus = [options_menu, abilities_menu]
+	current_menu = menus[selected_menu_index]
+	selected_button = current_menu[selected_index]
 	populate_abilities_menu()
-	update_selected_button()
 	cursor.move_cursor(Vector2i(0, 45))
 	
-func _input(e) -> void:
-	if Input.is_action_just_pressed("navigate_up"):
+func _input(_e) -> void:
+	if Input.is_action_just_pressed("navigate_backward"):
 		if selected_index == 0:
-			selected_index += (options_list.size() - 1)
+			selected_index += (current_menu.size() - 1)
 			update_selected_button()
 		else:
 			selected_index = (selected_index - 1)
 			update_selected_button()
-	elif Input.is_action_just_pressed("navigate_down"):
-		selected_index = (selected_index + 1) % options_list.size()
+	elif Input.is_action_just_pressed("navigate_forward"):
+		selected_index = (selected_index + 1) % current_menu.size()
 		update_selected_button()
 	elif Input.is_action_just_pressed("ui_accept"):
-		match selected_button.text:
-			"1. Attack":
-				on_attack_accept()
-			"2. Move":
-				pass
-			"3. Items":
-				pass
-			"4. Status":
-				pass
-			"5. Retreat":
-				pass
+		if selected_menu_index == 0:
+			match selected_button.text:
+				"1. Attack":
+					on_attack_accept()
+				"2. Move":
+					pass
+				"3. Items":
+					pass
+				"4. Status":
+					pass
+				"5. Retreat":
+					pass
+		else:
+			pass
 			
 func update_selected_button() -> void:
-	current_menu = menus[selected_menu_index]
-	if current_menu:
-		selected_button = current_menu[selected_index]
-	else:
-		selected_button = options_list[selected_index]
+	selected_button = current_menu[selected_index]
 	cursor.move_cursor(selected_button.position)
 	
 func populate_abilities_menu() -> void:
-	for i in range(abilities_list.size()):
-		if i <= 2:
-			abilities_list[i].text = str(i + 1) + ". " + character_abilities[i]
+	for i in range(abilities_menu.size()):
+		if i <= character_abilities.size() - 1:
+			abilities_menu[i].text = str(i + 1) + ". " + character_abilities[i]
 		else:
-			abilities_list[i].text = "???"
+			abilities_menu[i].text = "???"
 
 func on_attack_accept() -> void:
 	selected_menu_index = (selected_menu_index + 1) % menus.size()
 	current_menu = menus[selected_menu_index]
-	cursor.move_cursor(selected_button.position)
-	print(current_menu, selected_button)
+	cursor.set_menu_type(MenuType.ABILITIES)
+	update_selected_button()
