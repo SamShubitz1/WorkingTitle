1c65ec725944b544a1684ef693fa7eb379063aa2
Sam Shubitz <shubitzs@gmail.com>
2025-02-02 17:28:34 -0600

    tweaks

--- Scenes/Battle/Menu/battle_menu.gd
+++ Scenes/Battle/Menu/battle_menu.gd
@@ -3,19 +3,21 @@ extends Control
 @onready var cursor = $Cursor
 @onready var options_menu = $Options.get_children().slice(1)
 @onready var abilities_menu = $Abilities.get_children().slice(3)
+@onready var char_name_label = $Options/Name_Panel/Name_Label
 @onready var ability_description_label = $Descriptions/Ability_Description/Label
-var character_abilities: Array = [
+
+var character_info: Dictionary = {"name": "Deeno", "abilities": [
 	{"name":"Rock", "description": "A rock based attack"},
 	{"name": "Paper", "description": "A paper based attack"},
 	{"name": "Scissors", "description": "A scissors based attack"}
-	]
+	]}
 
 var selected_button_index: int = 0
 var selected_button: Button
 
-var menus: Array = []
+var menus: Array
 var selected_menu_index: int = 0
-var current_menu: Array
+var selected_menu: Array
 
 enum MenuType {
 	OPTIONS,
@@ -24,31 +26,33 @@ enum MenuType {
 
 func _ready() -> void:
 	menus = [options_menu, abilities_menu]
-	current_menu = menus[selected_menu_index]
-	selected_button = current_menu[selected_button_index]
+	selected_menu = menus[selected_menu_index]
+	selected_button = selected_menu[selected_button_index]
+	char_name_label.text = character_info.name
 	populate_abilities_menu()
 	cursor.move_cursor(Vector2i(0, 45))
 	
 func _input(_e) -> void:
 	if Input.is_action_just_pressed("navigate_backward"):
 		if selected_button_index == 0:
-			selected_button_index += (current_menu.size() - 1)
+			selected_button_index += (selected_menu.size() - 1)
 			update_selected_button()
 		else:
 			selected_button_index = (selected_button_index - 1)
 			update_selected_button()
 			
 	elif Input.is_action_just_pressed("navigate_forward"):
-		selected_button_index = (selected_button_index + 1) % current_menu.size()
+		selected_button_index = (selected_button_index + 1) % selected_menu.size()
 		update_selected_button()
 		
 	elif Input.is_action_just_pressed("go_back"):
 		if selected_menu_index == 1:
 			selected_menu_index = 0
 			selected_button_index = 0
-			current_menu = menus[selected_menu_index]
+			selected_menu = menus[selected_menu_index]
 			cursor.set_menu_type(MenuType.OPTIONS)
 			update_selected_button()
+			ability_description_label.text = "Sucker Punch"
 		
 	elif Input.is_action_just_pressed("ui_accept"):
 		if selected_menu_index == 0:
@@ -68,24 +72,24 @@ func _input(_e) -> void:
 			pass
 			
 func update_selected_button() -> void:
-	selected_button = current_menu[selected_button_index]
+	selected_button = selected_menu[selected_button_index]
 	cursor.move_cursor(selected_button.position)
 	
-	if current_menu == abilities_menu:
-		if selected_button_index <= character_abilities.size() - 1:
-			ability_description_label.text = character_abilities[selected_button_index].description
+	if selected_menu == abilities_menu:
+		if selected_button_index <= character_info.abilities.size() - 1:
+			ability_description_label.text = character_info.abilities[selected_button_index].description
 		else:
 			ability_description_label.text = "???"
 
 func on_attack_accept() -> void:
 	selected_menu_index = (selected_menu_index + 1) % menus.size()
-	current_menu = menus[selected_menu_index]
+	selected_menu = menus[selected_menu_index]
 	cursor.set_menu_type(MenuType.ABILITIES)
 	update_selected_button()
 	
 func populate_abilities_menu() -> void:
 	for i in range(abilities_menu.size()):
-		if i <= character_abilities.size() - 1:
-			abilities_menu[i].text += str(i + 1) + ". " + character_abilities[i].name
+		if i <= character_info.abilities.size() - 1:
+			abilities_menu[i].text += str(i + 1) + ". " + character_info.abilities[i].name
 		else:
 			abilities_menu[i].text = "???"
