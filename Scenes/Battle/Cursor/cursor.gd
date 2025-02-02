4379a3883e518555617216cca77cb235abbe850d
Sam Shubitz <shubitzs@gmail.com>
2025-02-02 17:31:31 -0600

    tweaks

--- Scenes/Battle/Cursor/cursor.gd
+++ Scenes/Battle/Cursor/cursor.gd
@@ -1,6 +1,6 @@
 extends TextureRect
 
-var current_menu_type: MenuType = MenuType.OPTIONS
+var selected_menu_type: MenuType = MenuType.OPTIONS
 var options_offset = Vector2i(110, 11)
 var abilities_offset = Vector2i(490, 21)
 
@@ -11,10 +11,10 @@ enum MenuType {
 
 #Enum types become their index?
 func move_cursor(button_position: Vector2i) -> void:
-	if current_menu_type == 0:
+	if selected_menu_type == 0:
 		self.position = button_position + options_offset
-	elif current_menu_type == 1:
+	elif selected_menu_type == 1:
 		self.position = button_position + abilities_offset
 
 func set_menu_type(type: MenuType):
-	current_menu_type = type
+	selected_menu_type = type
