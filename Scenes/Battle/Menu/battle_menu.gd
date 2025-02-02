5d9f721044229cea1991e021ed4327d6213e6120
Sam Shubitz <shubitzs@gmail.com>
2025-02-02 11:38:14 -0600

    cleaning

--- Scenes/Battle/Menu/battle_menu.gd
+++ Scenes/Battle/Menu/battle_menu.gd
@@ -3,17 +3,14 @@ extends Control
 @onready var cursor = $Cursor
 @onready var options_menu = $Options.get_children().slice(1)
 @onready var abilities_menu = $Abilities.get_children().slice(3)
-@onready var description_panels = $Descriptions.get_children()
-@onready var descriptions_menu = $Descriptions.get_children()
-@onready var ability_description_label = description_panels[1].get_node("Label")
-
+@onready var ability_description_label = $Descriptions/Ability_Description/Label
 var character_abilities: Array = [
 	{"name":"Rock", "description": "A rock based attack"},
 	{"name": "Paper", "description": "A paper based attack"},
 	{"name": "Scissors", "description": "A scissors based attack"}
 	]
 
-var selected_index: int = 0
+var selected_button_index: int = 0
 var selected_button: Button
 
 var menus: Array = []
@@ -28,27 +25,27 @@ enum MenuType {
 func _ready() -> void:
 	menus = [options_menu, abilities_menu]
 	current_menu = menus[selected_menu_index]
-	selected_button = current_menu[selected_index]
+	selected_button = current_menu[selected_button_index]
 	populate_abilities_menu()
 	cursor.move_cursor(Vector2i(0, 45))
 	
 func _input(_e) -> void:
 	if Input.is_action_just_pressed("navigate_backward"):
-		if selected_index == 0:
-			selected_index += (current_menu.size() - 1)
+		if selected_button_index == 0:
+			selected_button_index += (current_menu.size() - 1)
 			update_selected_button()
 		else:
-			selected_index = (selected_index - 1)
+			selected_button_index = (selected_button_index - 1)
 			update_selected_button()
 			
 	elif Input.is_action_just_pressed("navigate_forward"):
-		selected_index = (selected_index + 1) % current_menu.size()
+		selected_button_index = (selected_button_index + 1) % current_menu.size()
 		update_selected_button()
 		
 	elif Input.is_action_just_pressed("go_back"):
 		if selected_menu_index == 1:
 			selected_menu_index = 0
-			selected_index = 0
+			selected_button_index = 0
 			current_menu = menus[selected_menu_index]
 			cursor.set_menu_type(MenuType.OPTIONS)
 			update_selected_button()
@@ -71,12 +68,12 @@ func _input(_e) -> void:
 			pass
 			
 func update_selected_button() -> void:
-	selected_button = current_menu[selected_index]
+	selected_button = current_menu[selected_button_index]
 	cursor.move_cursor(selected_button.position)
 	
 	if current_menu == abilities_menu:
-		if selected_index <= character_abilities.size() - 1:
-			ability_description_label.text = character_abilities[selected_index].description
+		if selected_button_index <= character_abilities.size() - 1:
+			ability_description_label.text = character_abilities[selected_button_index].description
 		else:
 			ability_description_label.text = "???"
 
