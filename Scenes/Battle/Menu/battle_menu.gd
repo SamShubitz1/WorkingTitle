2f2dc74957b6ee491f9eb7266c120c9018da3f25
Sam Shubitz <shubitzs@gmail.com>
2025-02-11 22:54:14 -0600

    added rock paper scissors battle

--- Scenes/Battle/Menu/battle_menu.gd
+++ Scenes/Battle/Menu/battle_menu.gd
@@ -1,16 +1,20 @@
 extends Control
 
 @onready var cursor = $Cursor
-@onready var options_menu = $Main_Menu/Menu.get_children().slice(1)
-@onready var abilities_menu = $Abilities_Menu/Menu.get_children().slice(3)
-@onready var char_name_label = $Main_Menu/Menu/Name_Label
-@onready var ability_description_label = $Descriptions/Labels/Ability_Description
+@onready var options_menu = $MainMenu/Menu.get_children().slice(1)
+@onready var abilities_menu = $AbilitiesMenu/Menu.get_children().slice(3)
+@onready var char_name_label = $MainMenu/Menu/CharPanel/NameLabel
+@onready var ability_description_label = $Descriptions/Labels/AbilityDescription
+@onready var player_health = $MainMenu/Menu/CharPanel/Health
+@onready var enemy_health = $"../Enemy/EnemyHealth"
 
-var character_info: Dictionary = {"name": "Deeno", "abilities": [
+var character_info: Dictionary = {"name": "Deeno", "max_health": 100, "abilities": [
 	{"name":"Rock", "description": "A rock based attack"},
 	{"name": "Paper", "description": "A paper based attack"},
 	{"name": "Scissors", "description": "A scissors based attack"}
 	]}
+	
+var enemy_info: Dictionary = {"name": "Norman", "max_health": 80, "abilities": ["Rock","Paper","Scissors"]}
 
 var selected_button_index: int = 0
 var selected_button: Button
@@ -32,6 +36,8 @@ func _ready() -> void:
 	selected_button = selected_menu[selected_button_index]
 	char_name_label.text = character_info.name
 	populate_abilities_menu()
+	player_health.max_value = character_info["max_health"]
+	enemy_health.max_value = enemy_info["max_health"]
 	cursor.move_cursor(initial_cursor_position)
 	
 func _input(_e) -> void:
@@ -57,10 +63,10 @@ func _input(_e) -> void:
 			ability_description_label.text = "Text"
 		
 	elif Input.is_action_just_pressed("ui_accept"):
-		if selected_menu_index == 0:
+		if selected_menu == options_menu:
 			match selected_button.text:
 				"1. Attack":
-					on_select_attack()
+					on_select_abilities()
 				"2. Move":
 					pass
 				"3. Items":
@@ -70,8 +76,8 @@ func _input(_e) -> void:
 				"5. Retreat":
 					pass
 					
-		else:
-			pass
+		elif selected_menu == abilities_menu:
+			perform_attack(character_info.abilities[selected_button_index].name)
 			
 func update_selected_button() -> void:
 	selected_button = selected_menu[selected_button_index]
@@ -83,7 +89,7 @@ func update_selected_button() -> void:
 		else:
 			ability_description_label.text = "???"
 
-func on_select_attack() -> void:
+func on_select_abilities() -> void:
 	selected_menu_index = (selected_menu_index + 1) % menus.size()
 	selected_menu = menus[selected_menu_index]
 	cursor.set_menu_type(MenuType.ABILITIES)
@@ -95,3 +101,41 @@ func populate_abilities_menu() -> void:
 			abilities_menu[i].text += str(i + 1) + ". " + character_info.abilities[i].name
 		else:
 			abilities_menu[i].text = "???"
+			
+func perform_attack(player_attack: String) -> void:
+	var enemy_attack = get_enemy_attack()
+	if player_attack == "Rock":
+		match enemy_attack:
+			"Rock":
+				pass
+			"Paper":
+				player_health.value -= 20
+			"Scissors":
+				enemy_health.value -= 20
+	elif player_attack == "Paper":
+		match enemy_attack:
+			"Rock":
+				enemy_health.value -= 20
+			"Paper":
+				pass
+			"Scissors":
+				player_health.value -= 20
+	elif player_attack == "Scissors":
+		match enemy_attack:
+			"Rock":
+				player_health.value -= 20
+			"Paper":
+				enemy_health.value -= 20
+			"Scissors":
+				pass
+	check_death()
+			
+	
+func get_enemy_attack() -> String:
+	var enemy_attacks = ["Rock", "Paper", "Scissors"]
+	var attack_index = randi() % 3
+	return enemy_attacks[attack_index]
+	
+func check_death() -> void:
+	if player_health.value <= 0 || enemy_health.value <= 0:
+		get_tree().change_scene_to_file("res://Scenes/Main/mainscene.tscn") 
---
+++ Scenes/Battle/battle_controller.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+#selected an attack
+#enemy selects and attack
+#wee who won
+# take health away
