c4a70705300e482bfa42c1cb65a3e1290125e4b8
Sam Shubitz <shubitzs@gmail.com>
2025-03-01 15:34:43 -0600

    ...

--- Classes/base_grid_menu.gd
+++ Classes/base_grid_menu.gd
@@ -8,6 +8,13 @@ enum GridType {
 	PLAYER
 }
 
+enum Direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+}
+
 var targets_grid: Dictionary = {}
 var current_grid_type: GridType = GridType.GLOBAL
 const initial_grid_size: Vector2i = Vector2i(8, 4)
@@ -53,13 +60,59 @@ func get_selected_grid_coords() -> Vector2i:
 	var coords = targets_grid.find_key(selected_button)
 	if current_grid_type == GridType.ENEMY:
 		coords.x += initial_grid_size.x / 2 #assumes grid coords will always be 8x4
-	print(coords)
 	return coords
 
-func navigate_forward() -> void:
-	super.navigate_forward()
-	get_selected_grid_coords()
+func navigate_forward(e: InputEvent) -> void:
+	if e.keycode == KEY_DOWN || e.keycode == KEY_S:
+		move_down()
+	elif e.keycode == KEY_RIGHT || e.keycode == KEY_D:
+		move_right()
+			
+func navigate_backward(e: InputEvent) -> void:
+	if e.keycode == KEY_UP || e.keycode == KEY_W:
+		move_up()
+	elif e.keycode == KEY_LEFT || e.keycode == KEY_A:
+		move_left()
+			
+func move_up() -> void:
+	var coords = targets_grid.find_key(selected_button)
+	var next_coords: Vector2i
+	if coords.y == 3: # should be a dynamic value
+		next_coords = Vector2i(coords.x, 0)
+	else:
+		next_coords = Vector2i(coords.x, coords.y + 1)
+	selected_button = targets_grid[next_coords]
+	cursor.move_cursor(selected_button.position)
 
+func move_down() -> void:
+	var coords = targets_grid.find_key(selected_button)
+	var next_coords: Vector2i
+	if coords.y == 0: # should be a dynamic value
+		next_coords = Vector2i(coords.x, 3)
+	else:
+		next_coords = Vector2i(coords.x, coords.y - 1)
+	selected_button = targets_grid[next_coords]
+	cursor.move_cursor(selected_button.position)
+
+func move_left() -> void:
+	var coords = targets_grid.find_key(selected_button)
+	var next_coords: Vector2i
+	if coords.x == 0: # should be a dynamic value
+		next_coords = Vector2i(3, coords.y)
+	else:
+		next_coords = Vector2i(coords.x - 1, coords.y)
+	selected_button = targets_grid[next_coords]
+	cursor.move_cursor(selected_button.position)
+
+func move_right() -> void:
+	var coords = targets_grid.find_key(selected_button)
+	var next_coords: Vector2i
+	if coords.x == 3: # should be a dynamic value
+		next_coords = Vector2i(0, coords.y)
+	else:
+		next_coords = Vector2i(coords.x + 1, coords.y)
+	selected_button = targets_grid[next_coords]
+	cursor.move_cursor(selected_button.position)
+	
 func set_grid_type(type: GridType) -> void:
 	current_grid_type = type
-	
