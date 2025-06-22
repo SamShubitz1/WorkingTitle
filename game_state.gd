b44dfb61e0f527cfc582423bf93bfe944ee7f6f6
Sam Shubitz <shubitzs@gmail.com>
2025-06-22 10:52:54 -0500

    clean

--- game_state.gd
+++ game_state.gd
@@ -5,14 +5,15 @@ var current_weather: Weather = Weather.CLEAR
 var weather_change = current_time + 50
 
 func set_current_weather() -> void:
-	if current_time >= weather_change:
-		if current_weather == Weather.CLEAR:
-			current_weather = Weather.RAINING
-			print("YAY ITS RAININ BITCHES" , current_weather)
-		elif current_weather == Weather.RAINING:
-			current_weather = Weather.CLEAR
-			print("DAMN NO MORE BITCHES" , current_weather)
-		weather_change = current_time + 50
+	if current_time < weather_change:
+		return
+	if current_weather == Weather.CLEAR:
+		current_weather = Weather.RAINING
+		print("YAY ITS RAININ BITCHES" , current_weather)
+	elif current_weather == Weather.RAINING:
+		current_weather = Weather.CLEAR
+		print("DAMN NO MORE BITCHES" , current_weather)
+	weather_change = current_time + 50
 
 enum Weather {
 	CLEAR, 
