a4748d43deb6edb0afa20fa28284f76a6d300074
Sam Shubitz <shubitzs@gmail.com>
2025-07-04 12:08:10 -0500

    npc collision battle trigger if battle_ready

--- Rooms/T2/battle_perimeter.gd
+++ Rooms/T2/battle_perimeter.gd
@@ -6,5 +6,5 @@ func _ready():
 	self.area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.name == "MyPlayer":
+	if area.name == "MyPlayer" && get_parent().battle_ready == true:
 		game_controller.switch_to_scene(Data.Scenes.BATTLE, {"data": get_parent()})
