2d07fb8335bf9b38f571b4e14722374dcb08684e
henryphage <mikebolts@gmail.com>
2025-07-06 00:38:24 -0500

    smaller updates

--- Rooms/T2/aggro_area.gd
+++ Rooms/T2/aggro_area.gd
@@ -3,22 +3,22 @@ extends Area2D
 @onready var npc: BaseNPC = get_parent()
 
 var player_is_connected = false
+var player: Area2D
 
 func _ready():
 	self.area_entered.connect(_on_area_entered)
-	self.area_exited.connect(_on_area_exited)
-	
+
 func _on_area_entered(area: Area2D):
 	if area.name != "MyPlayer":
 		return
-	
+	player = area
 	if !player_is_connected:
 		var player_controller = area.get_parent()
-		player_controller.player_position_updated.connect(npc.set_player_coords)
+		player_controller.player_position_updated.connect(set_is_aggro)
 		player_is_connected = true
-	
-	npc.set_aggro_mode(true)
-	
-func _on_area_exited(area: Area2D):
-	if area.name == "MyPlayer":
+
+func set_is_aggro(args):
+	if self.overlaps_area(player):
+		npc.set_aggro_mode(true)
+	else:
 		npc.set_aggro_mode(false)
