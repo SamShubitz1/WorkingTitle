d789229bb3317c728a788056b011e304b3cc988b
Sam Shubitz <shubitzs@gmail.com>
2025-07-29 22:26:02 -0500

    push

--- Scenes/StartMenu/start_menu.gd
+++ Scenes/StartMenu/start_menu.gd
@@ -3,18 +3,16 @@ extends Control
 @onready var start_menu_cursor: BaseCursor = $Cursor
 @onready var pokemon_card = $PokemonCard
 @onready var pokemon_card_label = $PokemonCard/CardLabel
-@onready var main_menu = $MainMenu
 
 var menus: Array[BaseMenu] = []
 var selected_menu_index: int
 var selected_menu: BaseMenu
 var menu_history: Array
 
+var main_menu = BaseMenu.new()
 var pokedex_log = BaseLog.new()
-var current_selected_button: Node
+var current_selected_button: Button
 var cursor: BaseCursor
-
-var is_open: bool
 
 func init(menu_cursor: BaseCursor, current_menus: Array) -> void:
 	self.cursor = menu_cursor
@@ -25,34 +23,6 @@ func init(menu_cursor: BaseCursor, current_menus: Array) -> void:
 func _ready() -> void:
 	initialize_menus()
 	self.init(start_menu_cursor, menus)
-
-func open() -> void:
-	self.show()
-	is_open = true
-
-func close() -> void:
-	main_menu.reset()
-	self.hide()
-	is_open = false
-
-func _input(_e) -> void:
-	if is_open:
-		if Input.is_action_pressed("navigate_forward"):
-			selected_menu.navigate_forward(_e)
-			update_pokedex_entry()
-
-		elif Input.is_action_pressed("navigate_backward"):
-			selected_menu.navigate_backward(_e)
-			update_pokedex_entry()
-			
-		if Input.is_action_just_pressed("ui_accept"):
-			on_press_button()
-		
-		if Input.is_action_just_pressed("go_back"):
-			if selected_menu != main_menu:
-				selected_menu.hide_menu()
-				go_back()
-				pokemon_card.hide()
 
 func update_selected_menu(index: int) -> void:
 	if index != selected_menu_index:
@@ -67,17 +37,49 @@ func go_back() -> void:
 	if not menu_history.is_empty():
 		var prev_menu_index = menu_history.pop_back()
 		update_selected_menu(prev_menu_index)
+
+func open() -> void:
+	cursor.enable()
+	self.visible = true
+	
+func close() -> void:
+	cursor.disable()
+	self.visible = false
+
+func _input(_e) -> void:
+	if not cursor.disabled:
+		if Input.is_action_pressed("navigate_forward"):
+			color_off()
+			selected_menu.navigate_forward(_e)
+			update_pokedex_entry()
+			color_on()
+
+		elif Input.is_action_pressed("navigate_backward"):
+			color_off()
+			selected_menu.navigate_backward(_e)
+			update_pokedex_entry()
+			color_on()
+			
+		if Input.is_action_just_pressed("ui_accept"):
+			on_press_button()
+		
+		if Input.is_action_just_pressed("go_back"):
+			if selected_menu != main_menu:
+				selected_menu.hide_menu()
+				go_back()
+				pokemon_card.hide()
 				
 func on_press_button() -> void:
-	return
-	#if selected_menu == main_menu:
-		#var button_text = selected_menu.get_selected_button().text
-		#match button_text:
-			#"Pokédex":
-				#update_selected_menu(GameData.MainMenuType.POKEDEX)
-				#update_pokedex_entry()
-	#elif selected_menu == pokedex_log:
-		#pokemon_card.visible = !pokemon_card.visible
+	if selected_menu == main_menu:
+		var button_text = selected_menu.get_selected_button().text
+		color_off() 
+		match button_text:
+			"Pokédex":
+				update_selected_menu(GameData.MainMenuType.POKEDEX)
+				color_on()
+				update_pokedex_entry()
+	elif selected_menu == pokedex_log:
+		pokemon_card.visible = !pokemon_card.visible
 				
 func set_pokemon_card(entry) -> void:
 	var pokemon_info = GameData.pokedex[entry]
@@ -90,6 +92,13 @@ func list_types(types: Array) -> String:
 		type_string = str(type_string, " ", type)
 	return type_string
 	
+func color_on() -> void:
+	current_selected_button = selected_menu.get_selected_button()
+	current_selected_button.modulate = Color(1, 1, 0)
+	
+func color_off() -> void:
+	current_selected_button.modulate = Color(1, 1, 1)
+
 func update_pokedex_entry() -> void:
 	var entry = pokedex_log.get_current_entry()
 	set_pokemon_card(entry)
@@ -97,13 +106,11 @@ func update_pokedex_entry() -> void:
 func initialize_menus():
 	var menu_buttons = self.get_node("MainMenu/Menu").get_children()
 	var log_slots = self.get_node("PokedexLog/Menu").get_children()
-	
 	main_menu.init(self.get_node("MainMenu"), menu_buttons, start_menu_cursor)
 	main_menu.activate()
-	
 	pokedex_log.init(self.get_node("PokedexLog"), log_slots, start_menu_cursor, null, ["-","-","-"] + GameData.pokedex_entries)
 	pokedex_log.hide_menu()
-	
 	current_selected_button = main_menu.get_selected_button()
+	current_selected_button.modulate = Color(1, 1, 0)
 	menus = [main_menu, pokedex_log]
 	pokemon_card.hide()
