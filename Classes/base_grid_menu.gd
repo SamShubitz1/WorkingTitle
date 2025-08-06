4b9f2a3513003ce5f34921b6500f1c38870b5819
Sam Shubitz <shubitzs@gmail.com>
2025-08-05 20:10:00 -0500

    ...

--- Classes/base_grid_menu.gd
+++ Classes/base_grid_menu.gd
@@ -19,7 +19,6 @@ enum Direction {
 var battle_grid: Dictionary
 var targets_grid: Dictionary = {}
 var current_grid_type: GridType = GridType.GLOBAL
-var global_cells: Array[Node]
 var range_of_movement: Vector2i
 var origin: Vector2i
 var wrap: bool
@@ -30,10 +29,10 @@ var current_shape: GameData.AbilityShape
 #var custom_cells: Array
 var custom_index = 0
 	
-func init(menu: Node, buttons: Array, menu_cursor: BaseCursor, initial_button_position = null, wrap = true) -> void:
+func init(menu: Node, cells: Array, menu_cursor: BaseCursor, initial_button_position = null, wrap = true) -> void:
 	super.init(menu, buttons, menu_cursor)
 	self.wrap = wrap
-	global_cells = buttons
+	self.buttons = cells
 	update_grid(initial_grid_size)
 	for button in buttons:
 		button.modulate = Color(0, 0, 0, 0.1)
@@ -84,19 +83,19 @@ func activate_hero_grid() -> void:
 func get_enemy_cells() -> Array:
 	var enemy_cells: Array
 	var columns = initial_grid_size.x
-	for i in range(global_cells.size()):
+	for i in range(buttons.size()):
 		if i % columns == columns / 2:
 			for j in range(columns / 2):
-				enemy_cells.append(global_cells[i + j])
+				enemy_cells.append(buttons[i + j])
 	return enemy_cells
 				
 func get_player_cells() -> Array:
 	var player_cells: Array
 	var columns = initial_grid_size.x
-	for i in range(global_cells.size()):
+	for i in range(buttons.size()):
 		if i % columns == 0:
 			for j in range(columns / 2):
-				player_cells.append(global_cells[i + j])
+				player_cells.append(buttons[i + j])
 	return player_cells
 		
 func get_target_cells() -> Array:
